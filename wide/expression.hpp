c6fe02a785af623bf0311a0934d62f772a6a0d20
pd3v <paulo.develop@gmail.com>
2019-07-15 13:16:59 +0100

    'rotL' fix

--- wide/expression.hpp
+++ wide/expression.hpp
@@ -59,8 +59,6 @@ std::vector<int> rotR(vector<int>& notes) {
 std::vector<int> rotL(vector<int>& notes) {
   transform(notes.begin(),notes.end(),notes.begin(),[](int note){
     note += 1;
-    //note %= 12;
-    //note -= 12;
     
     if (note > 12)
       note = 0;
@@ -74,13 +72,12 @@ std::vector<int> rotL(vector<int>& notes) {
 }
 
 // ====================================
-// Scales
 
+// Scales
 scaleType Chromatic  {0,1,2,3,4,5,6,7,8,9,10,11};
 scaleType Major      {0,2,4,5,7,9,11};
 scaleType Minor      {0,2,3,5,7,9,11};
 
 //Chords
-
 chordType Maj  {0,2,4};
 chordType Min  {0,1,4};
