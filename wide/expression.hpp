44eb922da883160c9d0b5d6d89358879a2ffbb32
pd3v <paulo.develop@gmail.com>
2019-07-25 16:26:22 +0100

    new functions

--- wide/expression.hpp
+++ wide/expression.hpp
@@ -4,9 +4,7 @@
 //  Created by @pd3v_
 //
 
-#include <math.h>
 #include <vector>
-#include <chrono>
 #include <functional>
 #include <algorithm>
 
@@ -20,17 +18,32 @@ const T rnd(const T& max) {
 };
 
 template <typename T>
+const T range(const T& value,const T& max,const T& toMin,const T& toMax) {
+  return value/max*(toMax-toMin)+toMin;
+}
+
+template <typename T>
 const T rnd(const T& min,const T& max) {
   if (min >= 0 && max <= 1)
-    return 1.f/(rand()%10);
+    return range(static_cast<float>(rand()%10), 10.f, min, max);
   else
     return rand()%static_cast<int>(max-min)+min;
 };
 
-template <typename T>
-const T rndBunch(const vector<T> bunch) {
-  return bunch[rand()%bunch.size()];
+const int rndBunch(const vector<int> bunch) {
+  return static_cast<int>(bunch[rand()%bunch.size()]);
 }
+
+const unsigned long rndBunchDur(const vector<unsigned long> bunch) {
+  return static_cast<unsigned long>(bunch[rand()%bunch.size()]);
+}
+
+const float rndBunchAmp(const vector<float> bunch) {
+  return static_cast<float>(bunch[rand()%bunch.size()]);
+}
+
+auto rndBunchNotes = rndBunch;
+auto rndBunchOct = rndBunch;
 
 template <typename T>
 const vector<T> scramble(vector<T> n) {
@@ -39,23 +52,36 @@ const vector<T> scramble(vector<T> n) {
   return n;
 }
 
-template <typename T>
-const T range(const T& value,const T& max,const T& toMin,const T& toMax) {
-  return value/max*(toMax-toMin)+toMin;
+bool whenMod(int countTurn, unsigned long step) {
+  return step%countTurn == 0;
 }
 
-std::vector<int> shuffleOct(vector<int> vect,const vector<int> octaves) {
-  transform(vect.begin(),vect.end(),vect.begin(),[&](int n){
-    return octaves[rand()%octaves.size()]*12+n;
+vector<int> thisthat(unsigned long step, function<vector<int>()> thisFunc, function<vector<int>()> thatFunc, function<bool()> pred) {
+  return pred() ? thisFunc() : thatFunc();
+}
+
+int thisthat(unsigned long step, function<int()> thisFunc, function<int()> thatFunc, function<bool()> pred) {
+  return pred() ? thisFunc() : thatFunc();
+}
+
+// Not viable
+/*vector<int> shuffleoct(vector<int> vect,const vector<int> octaves) {
+  transform(vect.begin(),vect.end(),vect.begin(),[&octaves](int n){
+    n = octaves[rand()%octaves.size()]*12+n;
+    return n;
   });
-
   return vect;
+}*/
+
+template <typename T>
+T cycle(vector<T> v, unsigned long step) {
+  return v.at(step%v.size());
 }
 
-std::vector<int> rotR(vector<int>& notes) {
-  transform(notes.begin(),notes.end(),notes.begin(),[](int note){
+vector<int> rotR(vector<int>& notes, vector<int> scale) {
+  transform(notes.begin(),notes.end(),notes.begin(),[&scale](int note){
     note += 1;
-    note %= 12;
+    note %= scale.size();
     
     return note;
   });
@@ -63,18 +89,32 @@ std::vector<int> rotR(vector<int>& notes) {
   return notes;
 }
 
-std::vector<int> rotL(vector<int>& notes) {
-  transform(notes.begin(),notes.end(),notes.begin(),[](int note){
+// rotR with with scale param on currying
+// this, to work, would required some structural changes in Instrument
+/*function<vector<int>(vector<int>)> rotR(vector<int>& notes) {
+  return
+  [&notes](vector<int> scale)->vector<int>{
+    transform(notes.begin(),notes.end(),notes.begin(),[&scale](int note){
+      note += 1;
+      note %= scale.size();
+      return note;
+    });
+    
+    return notes;
+  };
+}*/
+
+vector<int> rotL(vector<int>& notes, vector<int> scale) {
+  transform(notes.begin(),notes.end(),notes.begin(),[&scale](int note){
     note -= 1;
+    note %= static_cast<int>(scale.size());
     
-    if (note > 12)
-      note = 0;
-    else if (note < 0)
-      note = 11;
-    
+    if (note < 0)
+      note = static_cast<int>(scale.size()-1);
+  
     return note;
   });
-  
+
   return notes;
 }
 
