f2f0f159acd12f87067d03c70c227f080f18b5a5
pd3v <paulo.develop@gmail.com>
2019-07-30 16:36:50 +0100

    examples

---
+++ examples/1.cpp
@@ -0,0 +1,13 @@
+
+
+// Play c4 note in instrument 0, amplitude 1, 1/4 duration notes, octave 4.
+i(0).play(
+  n(
+    ({0}),1,4,4
+    ))
+
+// Play c Major chord in instrument 0, amplitude 1, 1/16 duration notes, octave 5.
+i(0).play(
+  n(
+    ({0,2,4,6}),1,16,5
+    ))
