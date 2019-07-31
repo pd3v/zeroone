b613063dc968c38c77876da2c958c1600a916aff
pd3v <paulo.develop@gmail.com>
2019-07-31 12:48:47 +0100

    create a chord

--- examples/1.cpp
+++ examples/1.cpp
@@ -1,13 +1,23 @@
 
+// Play C4 note in instrument 0, amplitude 1, 1/4 duration notes, octave 4.
 
-// Play c4 note in instrument 0, amplitude 1, 1/4 duration notes, octave 4.
 i(0).play(
   n(
     ({0}),1,4,4
-    ))
+  ))
 
-// Play c Major chord in instrument 0, amplitude 1, 1/16 duration notes, octave 5.
+// Play C Major 7 chord in instrument 0, amplitude 1, 1/16 duration notes, octave 5.
+
 i(0).play(
   n(
     ({0,2,4,6}),1,16,5
-    ))
+  ))
+
+// Or
+
+chordType Maj7 {0,2,4,6};
+
+i(0).play(
+  n(
+    (Maj7),1,4,5
+  ))
