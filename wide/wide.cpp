995cae51d10ceac8fe96701378b27f7996d833f2
pd3v <paulo.develop@gmail.com>
2020-01-29 13:57:26 +0000

    remove notes print

--- wide/wide.cpp
+++ wide/wide.cpp
@@ -169,7 +169,7 @@ public:
     if (notes.dur.size() == 1) {
       notes.dur.resize(BAR_DUR_REF/duration[(notes.dur.front())]);
       fill(notes.dur.begin(),notes.dur.end(),notes.dur.front());
-      notes.print();
+      
       return notes.dur;
     }
     
