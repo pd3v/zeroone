83d5315df7d52cd9a74d0d7e97dd856a2711c629
pd3v <paulo.develop@gmail.com>
2021-04-09 15:38:21 +0100

    yield threads as way to sync them

--- wide/instrument.hpp
+++ wide/instrument.hpp
@@ -9,10 +9,11 @@
 #include <vector>
 #include <functional>
 #include <math.h>
+#include <numeric>
 #include "notes.hpp"
 #include "generator.hpp"
-#include "/Volumes/Data/Xcode Projects/chronometer/chronometer/chronometer.h"
 
+extern const uint16_t NUM_TASKS;
 extern const function<Notes()> SILENCE;
 extern const vector<function<CC()>> NO_CTRL;
 
@@ -87,90 +88,28 @@ public:
     return _mute;
   }
   
-  std::string hey() {
-    std::string ah = "this is ";
-    ah += to_string(id);
-    ah += ". Hey!";
-    
-    return ah;
-  }
-  
-  void wait(bool _wait) {
-    this->_wait = _wait;
-  }
-  
-  bool wait() {
-    return _wait;
-  }
-  
-  void chrono(long long _chrono) {
-    this->_chrono = _chrono;
-  }
-  
-  long long chrono() {
-    return _chrono;
-  }
-  
   int id;
   uint32_t step = 0, ccStep = 0;
   shared_ptr<function<Notes(void)>> const f = make_shared<function<Notes(void)>>(SILENCE);
   shared_ptr<vector<function<CC(void)>>> const ccs = make_shared<vector<function<CC(void)>>>(NO_CTRL);
   Notes out = {{0},0.,{1},1};
-  bool _wait = false;
-  long long _chrono = 0;
   
 private:
   int _ch;
   bool _mute = false;
   bool recurCCStart = true;
   vector<function<CC()>> _ccs{};
-//  bool _wait = false;
 };
 
-
-
-
-// Metro, an instrument wrapper struct with
-// the specific job of being a metronome
-
-struct _Metro {
-public:
-  static int tickPrecision;
-  static Instrument* inst;
-  
-  static void setInst(Instrument& _inst) {
-    inst = &_inst;
-    
-    // Instrument working as a metronome and ticks 64 times per bar
-    function<Notes()> beatFunc = [&]()->Notes {return {(vector<int>{}),0,{tickPrecision},1};};
-    inst->play(beatFunc);
-  }
-  
-  static void setTick(int _tickPrecision) {
-    tickPrecision = _tickPrecision;
-  }
-  
-  //FIXME: this range conversion sometimes returns the same value for two contiguous steps
-  static uint32_t sync(int timeSignature) {
-    return floor(inst->step*(timeSignature/static_cast<float>(tickPrecision)));
-  }
-  
-  static uint32_t playhead() {
-    return sync(4);
-  }
-};
-
-Instrument* _Metro::inst = nullptr;
-int _Metro::tickPrecision = 64;
-
-// Metronome standalone, independent thread
+// Metronome standalone, in an independent thread
 struct Metro {
 public:
-  static vector<Instrument>* insts;
-  static condition_variable cv;
   static uint32_t step;
   static bool on;
-  static bool waitOverall;
+  static atomic<bool> yieldInstTask;
+  static std::vector<Instrument> *insts;
+  static std::vector<long> instsWaitingTimes;
+  static unsigned long startTime, elapsedTime, barStartTime;
   
   static void setTick(int _tickPrecision) {
     tickPrecision = _tickPrecision;
@@ -182,51 +121,46 @@ public:
   
   static void start() {
     on = true;
+    long t = 0;
     
     std::thread([&](){
-      //int waitCount = 0;
-      
       while (on) {
+        startTime = chrono::time_point_cast<chrono::microseconds>(chrono::steady_clock::now()).time_since_epoch().count();
+        
+        if (step == 0)
+          yieldInstTask.store(false);
+        
         step++;
-        /*
-        waitCount = 0;
-        waitOverall = false;
         
-        for_each(insts->begin(),insts->end(),[&](auto& i){waitCount += (i.wait() == true) ? 1 : 0;});
+        elapsedTime = chrono::time_point_cast<chrono::microseconds>(chrono::steady_clock::now()).time_since_epoch().count();
+        t = static_cast<unsigned int>((Generator::barDurMs())/tickPrecision)-(elapsedTime-startTime);
+        t = (t >= 0 ? t : 0);
         
-        if (waitCount == NUM_TASKS)
-          waitOverall = true;
-        
-        if (waitOverall) {
-          cout << "entered notify.all()" << endl;
-          std::lock_guard<std::mutex> lock (TaskPool<SJob>::mtx);
-          waitOverall = false;
-          for_each(insts->begin(),insts->end(),[](auto& i){i.wait(false);});
-          cv.notify_all();
-        }
-        */
-        
-        this_thread::sleep_for(chrono::microseconds(Generator::barDurMs()/tickPrecision));
-        
-//        if (step%(tickPrecision*4) == 0) {
-        if (step%tickPrecision == 0) {
-          auto ah = chrono::time_point_cast<chrono::microseconds>(chrono::steady_clock::now()).time_since_epoch().count();
-          cout << step << " " << ah << " " << insts->at(0).chrono() << " " << (ah-insts->at(0).chrono())/1000. << endl;
-        }
-        
-        
+        this_thread::sleep_for(chrono::microseconds(t));
       }
     }).detach();
-  }
-  
-  static void notify() {
-    for_each(insts->begin(),insts->end(),[](auto& i){i.wait(false);});
-    cv.notify_all();
   }
   
   static void stop() {
     on = false;
     step = 0;
+    TaskPool<SJob>::yieldTaskCntr.store(0);
+  }
+  
+  // will attempt to metro sync SJobs' tasks/threads
+  static int syncInstTask(int taskId) {
+    TaskPool<SJob>::yieldTaskCntr.store(++TaskPool<SJob>::yieldTaskCntr);
+    
+    while (TaskPool<SJob>::yieldTaskCntr.load() >= 1 && TaskPool<SJob>::yieldTaskCntr.load() <= NUM_TASKS) {
+      this_thread::yield();
+      if (TaskPool<SJob>::yieldTaskCntr.load() == NUM_TASKS) TaskPool<SJob>::yieldTaskCntr.store(0);
+    }
+    return taskId;
+  }
+
+  static long minWaitingTime() {
+    long minWait = *min_element(instsWaitingTimes.begin(),instsWaitingTimes.end());
+    return (minWait < 0 ? 0 : minWait);
   }
   
   //FIXME: this range conversion sometimes returns the same value for two contiguous steps
@@ -242,10 +176,10 @@ private:
   static uint16_t tickPrecision;
 };
 
-vector<Instrument>* Metro::insts = nullptr;
-condition_variable Metro::cv;
-uint16_t Metro::tickPrecision = 256;
+uint16_t Metro::tickPrecision = 64;
 uint32_t Metro::step = 0;
 bool Metro::on = true;
-bool Metro::waitOverall = false;
-
+atomic<bool> Metro::yieldInstTask(true); // on start sync
+unsigned long Metro::startTime = 0, Metro::elapsedTime = 0, Metro::barStartTime = 0;
+std::vector<Instrument> *Metro::insts = nullptr;
+std::vector<long> Metro::instsWaitingTimes(NUM_TASKS,0);
