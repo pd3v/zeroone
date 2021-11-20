8f6e27dec338977bc14ed0abeb39f89f324d5b24
pd3v <paulo.develop@gmail.com>
2021-11-20 16:19:51 +0000

    re-path

--- build.sh
+++ build.sh
@@ -2,7 +2,12 @@
 
 mkdir build
 cd build
-cmake ..
+
+# Not using JACK
+cmake -DRTMIDI_API_JACK=OFF RTMIDI_BUILD_TESTING=OFF ..
+
+# Using JACK
+#cmake -DRTMIDI_BUILD_TESTING=OFF ..
+
 make
-cd zeroone
 cling -std=c++14
